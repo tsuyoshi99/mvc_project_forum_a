17ed91f4b11f332516e61d7f88276f439d17c710
Visal Yun <yunvisal18@kit.edu.kh>
2020-03-02 12:34:47 +0700

    modification on controller

--- src/Controller/StudentController.java
+++ src/Controller/StudentController.java
@@ -6,16 +6,21 @@ import com.google.gson.Gson;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.List;
 
+@WebServlet("/studentController/*")
 public class StudentController extends HttpServlet {
     Student student = null;
     StudentModel studentModel = null;
+    Gson gson = null;
 
     boolean result;
     //create StudentDAO class
@@ -24,12 +29,167 @@ public class StudentController extends HttpServlet {
         studentModel = StudentModel.getStudentModel();
     }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { //can be doPost base on UI design
-        //TO-DO:
+    private void sendAsJson(HttpServletResponse response, Object obj) throws IOException{
+        response.setContentType("application/json");
+        String res = gson.toJson(obj);
+
+        PrintWriter out = response.getWriter();
+        out.print(res);
+        out.flush();
     }
 
-    public void insertStudent(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+    // GET/mvc_project_forum_a/studentController/
+    // GET/mvc_project_forum_a/studentController/id
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //getting URL pattern
+        student = null;
+        String pathInfo = req.getPathInfo();
+
+        //if URL pattern didn't match
+        if(pathInfo == null || pathInfo.equals("/")){
+            sendAsJson(resp, student);
+            return;
+        }
+
+        //split URL pattern in order to get id
+        String[] urlSplits = pathInfo.split("/");
+        if(urlSplits.length!=2){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        //check if there an id that match
+        int id = Integer.parseInt(urlSplits[1]);
+        try{
+            String result = checkStudent(req,resp,id);
+            if(result=="null"){
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        //get the student object and send as JSON file
+        try {
+            student = getSingleStudent(req,resp,id);
+            sendAsJson(resp, student);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // POST/mvc_project_forum_a/studentController/
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //getting URL pattern
+        student = null;
+        String pathInfo = req.getPathInfo();
+
+        //read Http request body
+        if(pathInfo == null || pathInfo.equals("/")){
+            StringBuilder buffer = new StringBuilder();
+            BufferedReader reader = req.getReader();
+            String reqline;
+            while ((reqline = reader.readLine()) != null) {
+                buffer.append(reqline);
+            }
+            //convert Http request body to JSON
+            String payload = buffer.toString();
+            student = gson.fromJson(payload, Student.class);
+        }
+        //insert into DB and send response as JSON
+        try {
+            studentModel.insert(student);
+            sendAsJson(resp,student);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // PUT/mvc_project_forum_a/studentController/id
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //check URL pattern
+        student = null;
+        String pathInfo = req.getPathInfo();
+
+        if(pathInfo == null || pathInfo.equals("/")){
+            sendAsJson(resp, student);
+            return;
+        }
+
+        //split URL pattern in order to get id
+        String[] urlSplits = pathInfo.split("/");
+        if(urlSplits.length!=2){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        //check if there an id that match
+        int id = Integer.parseInt(urlSplits[1]);
+        try{
+            String result = checkStudent(req,resp,id);
+            if(result=="null"){
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        //read http request body
+        StringBuilder buffer = new StringBuilder();
+        BufferedReader reader = req.getReader();
+        String reqline;
+        while ((reqline = reader.readLine()) != null) {
+            buffer.append(reqline);
+        }
+        //convert to student object
+        String payload = buffer.toString();
+        student = gson.fromJson(payload, Student.class);
+
+        //update in DB and send response as JSON
+        try {
+            studentModel.update(student);
+            sendAsJson(resp,student);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // DELETE/mvc_project_forum_a/studentController/id
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //check URL pattern
+        student = null;
+        String pathInfo = req.getPathInfo();
+
+        if(pathInfo == null || pathInfo.equals("/")){
+            sendAsJson(resp, student);
+            return;
+        }
+
+        //split URL pattern in order to get id
+        String[] urlSplits = pathInfo.split("/");
+        if(urlSplits.length!=2){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        //delete in DB and send response as JSON
+        int id = Integer.parseInt(urlSplits[1]);
+        try {
+            student = getSingleStudent(req,resp,id);
+            studentModel.delete(id);
+            sendAsJson(resp, student);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*public Student insertStudent(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
         //get user input
         int id = Integer.parseInt(request.getParameter(""));
         String name = request.getParameter("");
@@ -41,30 +201,20 @@ public class StudentController extends HttpServlet {
         result = studentModel.insert(student);
 
         if(result){
-            //do sth
+            return student;
         }
         else{
-            //do sth
+            return null;
         }
-    }
+    }*/
 
-    public void getSingleStudent(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
-        //get Id from input
-        String id = request.getParameter("");
+    public Student getSingleStudent(HttpServletRequest request, HttpServletResponse response, int id) throws ServletException, IOException, SQLException {
         //fetch student record
-        student = studentModel.get(Integer.parseInt(id));
-        request.setAttribute("employee", student);
-
-        //convert to JSON file
-        Gson gson = new Gson();
-        String json = gson.toJson(student);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-
-        //handle the JSON text here
+        student = studentModel.get(id);
+        return student;
     }
 
-    public void getListStudent(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+    /*public void getListStudent(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
         //fetch all record and store as list
         List<Student> studentsList = studentModel.get();
         request.setAttribute("list", studentsList);
@@ -76,22 +226,22 @@ public class StudentController extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
 
         //handle the JSON text here
-    }
+    }*/
 
-    public void checkStudent(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+    public String checkStudent(HttpServletRequest request, HttpServletResponse response, int id) throws ServletException, IOException, SQLException {
         //get Id from input
-        String id = request.getParameter("");
+        //String id = request.getParameter("");
         //fetch student record
-        student = studentModel.get(Integer.parseInt(id));
+        student = studentModel.get(id);
         if(student==null){
-            //do sth
+            return "null";
         }
         else{
-            //do sth
+            return "true";
         }
     }
 
-    public void updateUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+    /*public void updateUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
         int id = Integer.parseInt(request.getParameter(""));
         String name = request.getParameter("");
         String DOB = request.getParameter("");
@@ -106,9 +256,9 @@ public class StudentController extends HttpServlet {
         else{
             //do sth
         }
-    }
+    }*/
 
-    public void deleteUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+    /*public void deleteUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
         int id = Integer.parseInt(request.getParameter("id"));
         int result = studentModel.delete(id);
 
@@ -118,5 +268,5 @@ public class StudentController extends HttpServlet {
         else{
             //do sth
         }
-    }
+    }*/
 }
